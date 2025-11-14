acee096f04dd6c436822b89bd9ead86c87be576e
robel kiros <robelalbeta@gmail.com>
2025-11-15 02:41:33 +0300

    Add InventoryPage, CartPage, CheckoutPage skeletons and update tests

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,5 +1,32 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CartPage {
 
+    private WebDriver driver;
+
+    // Locators
+    private By addToCartButton = By.id("add-to-cart-sauce-labs-backpack");
+    private By shoppingCartLink = By.className("shopping_cart_link");
+    private By cartItem = By.className("cart_item");
+
+    // Constructor
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void addBackpackToCart() {
+        driver.findElement(addToCartButton).click();
+    }
+
+    public void openCart() {
+        driver.findElement(shoppingCartLink).click();
+    }
+
+    public boolean isItemInCart() {
+        return driver.findElements(cartItem).size() > 0;
+    }
 }
--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,5 +1,24 @@
 package tests;
 
-public class CartTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.LoginPage;
 
+public class CartTest extends BaseTest {
+
+    @Test
+    public void addItemToCartTest() {
+        // Login first
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+
+        // Add item to cart
+        CartPage cartPage = new CartPage(driver);
+        cartPage.addBackpackToCart();
+        cartPage.openCart();
+
+        // Verify item is in cart
+        Assert.assertTrue(cartPage.isItemInCart(), "Item was not added to cart!");
+    }
 }
